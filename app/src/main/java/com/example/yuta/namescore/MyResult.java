48166b15c3c9abe602c0bad3ec778b77e30ef200
yuta1010 <riead.fashion@gmail.com>
2014-11-09 23:38:07 +0900

    final

---
+++ app/src/main/java/com/example/yuta/namescore/MyResult.java
@@ -0,0 +1,55 @@
+package com.example.yuta.namescore;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+public class MyResult extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_result);
+
+        Intent intent = getIntent();
+        String myName = intent.getStringExtra(Myform.EXTRA_MYNAME);
+        TextView myLabel =  (TextView)  findViewById(R.id.myLabel);
+        myLabel.setText(myName + "の点数は。。。");
+
+        Random randomGenerator = new Random();
+        int score = randomGenerator.nextInt(101);
+        String result = Integer.toString(score);
+        TextView myResult = (TextView) findViewById(R.id.myResult);
+        myResult.setText(result + "点!!!");
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my_result, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/yuta/namescore/Myform.java
+++ app/src/main/java/com/example/yuta/namescore/Myform.java
@@ -1,23 +1,76 @@
 package com.example.yuta.namescore;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Myform extends Activity {
 
+    //staticはclassの中
+    public final static  String EXTRA_MYNAME = "com.example.yuta.namescore.MYNAME";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_myform);
+        //これ以降に書く setContentView以下
+
+        final EditText myEditText = (EditText) findViewById(R.id.myEditText);
+
+
+        //CLICKしたとき
+        Button button = (Button) findViewById(R.id.myButton);
+         button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getScore(myEditText);
+            }
+        });
+
     }
 
-    protected void getScore(View v){
+    public void getScore(EditText myEditText) {
+        String myName = myEditText.getText().toString().trim();
+        if (myName.equals("")) {
+            //エラー表示
 
-    }
+            //1. seterror (EditTextの中に文字が浮かぶ)
+            myEditText.setError("Please Enter Your Name");
+
+            //2.Toast (下にダイアログが浮かぶ)
+            //Toast.makeText(this, "Please Enter Your Name", Toast.LENGTH_LONG).show();
+
+            //3. Dialog (真ん中にダイアログが浮かぶ)
+        /*    AlertDialog.Builder alertDialogBuilder = new  AlertDialog.Builder(this);
+            alertDialogBuilder
+                        .setTitle("Error!")
+                        .setMessage("Please Enter Your Name")
+                        .setPositiveButton("OK",null);
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+            */
+        }
+
+        else {
+        }
+        //次の画面へ
+        Intent intent = new Intent(this,MyResult.class);
+        intent.putExtra(EXTRA_MYNAME,myName);
+        startActivity(intent);
+        }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
